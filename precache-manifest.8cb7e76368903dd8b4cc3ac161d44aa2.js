ec38ba00153d7013cefd1f9fed12257c09247b70
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-16 16:01:43 +0000

    Deploy NerradSamoht/wp-site-crawler to github.com/NerradSamoht/wp-site-crawler.git:gh-pages

---
+++ precache-manifest.8cb7e76368903dd8b4cc3ac161d44aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3d5fe69746110cbab4f",
+    "url": "/wp-site-crawler/static/css/main.d0a63fa1.chunk.css"
+  },
+  {
+    "revision": "a3d5fe69746110cbab4f",
+    "url": "/wp-site-crawler/static/js/main.55554bf7.chunk.js"
+  },
+  {
+    "revision": "0071d5fbfa3854b9d91e",
+    "url": "/wp-site-crawler/static/js/runtime~main.2b88834a.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/wp-site-crawler/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "12137e52018265593e69327ada4afff8",
+    "url": "/wp-site-crawler/index.html"
+  }
+];
